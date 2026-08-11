274496f1f409b9856e294dd91cad91a29ac63bb3
Ritu-Bisen <ritu2bisen@gmail.com>
2026-08-11 17:57:53 +0530

    audit issue fix

--- src/routes/returnToWarehouseRoutes.js
+++ src/routes/returnToWarehouseRoutes.js
@@ -4,14 +4,5 @@ const returnToWarehouseController = require('../controllers/returnToWarehouseCon
 const { authenticate } = require('../middleware/authMiddleware');
 
 router.get('/', authenticate, returnToWarehouseController.getReturnToWarehouseList);
-router.get('/warehouses', authenticate, returnToWarehouseController.getWarehouses);
-router.get('/:id', authenticate, returnToWarehouseController.getHeader);
-router.post('/', authenticate, returnToWarehouseController.createHeader);
-router.put('/:id', authenticate, returnToWarehouseController.updateHeader);
-router.get('/:id/items', authenticate, returnToWarehouseController.getItems);
-router.post('/:id/items', authenticate, returnToWarehouseController.addItem);
-router.put('/items/:itemId', authenticate, returnToWarehouseController.updateItem);
-router.delete('/items/:itemId', authenticate, returnToWarehouseController.deleteItem);
-router.post('/:id/complete', authenticate, returnToWarehouseController.completeIssue);
 
 module.exports = router;
